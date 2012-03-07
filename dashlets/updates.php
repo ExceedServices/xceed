e126ea2afd15017a31828d37ca7f45f86e6fc55b
Owen Johnson <owen@owenjohnson.info>
2012-03-06 22:14:02 -0800

    Good updater fix

--- dashlets/updates.php
+++ dashlets/updates.php
@@ -5,12 +5,12 @@ require_once("roles.php");
 
 <?php 
     exec("git fetch origin");
-    $gitOutput = exec("git diff --shortstat origin/master");
+    $gitOutput = exec("git diff --shortstat origin/master master");
     if (strlen(trim($gitOutput))!=0)
     {
     ?><h3>Dashboard Software Update</h3>
 <div id="updates-content">
-       <p><?php echo($gitOutput); ?>
+       <p>Updates are availible!</p>
        <p>Would you like to instantly install these updates? <input type="submit" value="Do Upgrade" onClick = "$('#updates-content').load('ajax/do-updates.php');" /></p>
     <?php }
 ?>
