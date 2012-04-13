da5642ebd395036445ee2c04e4d5301a44127b3c
Owen Johnson <owen@owenjohnson.info>
2012-04-12 20:40:08 -0700

    Contacts Search

---
+++ dashlets/contacts.php
@@ -0,0 +1,8 @@
+<h3>Contacts</h3>
+<form>
+<input id = "contacts-search" type = "text" name = "q" placeholder = "Search" autocomplete="off" />
+<input id = "contacts-add" type = "submit" value = "Add" />
+</form>
+<div id="contacts-search-results" ></div>
+<div id="contacts-search-detail"></div>
+
