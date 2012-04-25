e95f160eb279e869cbbf41e36261278c366180f0
Owen Johnson <owen@owenjohnson.info>
2012-04-24 22:26:50 -0700

    Commit from dashboard shows who commited now

--- autocommit.php
+++ autocommit.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['id']))
 
     passthru("git status");
     passthru("git add *");
-    passthru('git commit -m "'. $_REQUEST['message'] .'" --author '. $_SESSION['name'] . ' <' . $_SESSION['email'].'>' );
+    passthru('git commit -m "'. $_REQUEST['message'] .'" --author "'. $_SESSION['name'] . ' <' . $_SESSION['email'].'>"' );
     passthru("git pull");
     passthru("git push");
 ?>
