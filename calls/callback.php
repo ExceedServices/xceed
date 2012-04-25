42a102efc977bfb26cae01d14e08c2431798d505
Owen Johnson <owen@owenjohnson.info>
2012-04-25 11:38:38 -0500

    Update calls/callback.php

--- calls/callback.php
+++ calls/callback.php
@@ -4,6 +4,6 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say>Hello <?php echo $_SESSION['name'] ?>, let me find your customer!</Say>
+    <Say>Let me find that customer for you!</Say>
     <Dial><?php echo $_REQUEST['number']?></Dial>
 </Response>
