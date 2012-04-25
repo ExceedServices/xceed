2eb7d228a522e529db5832090277628c176140ea
Owen Johnson <owen2@stanley.dreamhost.com>
2012-04-24 22:11:35 -0700

    Fixed commit from dashboard

--- autocommit.php
+++ autocommit.php
@@ -1,7 +1,7 @@
 <?php 
     passthru("git status");
     passthru("git add *");
-    passthru('git commit -m "'. urlencode($_REQUEST['message']) .'"');
+    passthru('git commit -m "'. $_REQUEST['message'] .'"');
     passthru("git pull");
     passthru("git push");
 ?>
