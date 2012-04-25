e05a051e8a2491972ebf82647da62c543ddf5228
Owen Johnson <owen@owenjohnson.info>
2012-04-24 22:25:58 -0700

    Fixed commit from dashboard

--- autocommit.php
+++ autocommit.php
@@ -1,7 +1,13 @@
 <?php 
+    if(!isset($_SESSION))
+    session_start();
+
+if (!isset($_SESSION['id']))
+    header('location: /');
+
     passthru("git status");
     passthru("git add *");
-    passthru('git commit -m "'. $_REQUEST['message'] .'"');
+    passthru('git commit -m "'. $_REQUEST['message'] .'" --author '. $_SESSION['name'] . ' <' . $_SESSION['email'].'>' );
     passthru("git pull");
     passthru("git push");
 ?>
