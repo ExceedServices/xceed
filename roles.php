c75d16a1d00d292d2622291b73785c4d681f831f
Owen Johnson <owen@owenjohnson.info>
2012-05-17 16:18:06 -0700

    Fixed security bug in which roles that are substrings of other roles are given to the wrong people.

--- roles.php
+++ roles.php
@@ -4,7 +4,7 @@ require_once("connect.php");
 
 function hasRole($role)
 {
-    return (strpos($_SESSION['roles'], $role));
+    return (strpos($_SESSION['roles'], "|".$role."|"));
 }
     
 function addRole($userID, $role)
