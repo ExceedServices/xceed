a38295426c7ca6f8dd96f7a1bfb89c24f595259e
Adam Bourland <adam.bourland@gmail.com>
2012-06-12 20:51:12 -0700

    fixed save.php bug

--- saver.php
+++ saver.php
@@ -8,9 +8,9 @@ if (!isset($_REQUEST['field']) || !isset($_REQUEST['table']) || !isset($_REQUEST
 $table = mysql_real_escape_string($_REQUEST['table']);
 $field = mysql_real_escape_string($_REQUEST['field']);
 $value = mysql_real_escape_string($_REQUEST['value']);
-$key   = mysql_real_escape_string()$_REQUEST['key']);
+$key   = mysql_real_escape_string($_REQUEST['key']);
 
-$q = "update $table set $field = '$value' where id = $key";
+$q = "update $table set $field = '$value' where id = '$key'";
 
 $success = mysql_query($q);
 if(!$success)
