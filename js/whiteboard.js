9ea91fc2282438ea078f3bb71aecc5609fffdd74
Owen Johnson <owen@owenjohnson.info>
2012-06-12 21:42:17 -0500

    added dynamic saver
    it's seriously cool

--- js/whiteboard.js
+++ js/whiteboard.js
@@ -12,5 +12,24 @@ $(document).ready(function(){
         }
     }
     $(this).load("loader.php?dashlet="+$(this).attr("data-loader")+ args);
-  })
+  });
+
+      $('body').click(function(event) 
+    {
+         if($(event.target).is('input[data-savable]'))
+         {
+            $(event.target).removeClass("editable");
+            $(event.target).blur(function(event)
+            {
+                var payload = new Object();
+                payload.table = $(event.target).attr("data-table");
+                payload.field = $(event.target).attr("data-field");
+                payload.key = $(event.target).attr("data-key");
+                payload.value = $(event.target).val();
+                $.post('saver.php',payload, function(data){$(event.target).addClass("editable")});
+            });
+        }
+    });
 });
+
+
