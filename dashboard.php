b947e77446bc97a2a1c82ccd3a49a2c4e365f885
Adam <bourlaad@mitchell.dreamhost.com>
2014-08-20 19:12:52 -0700

    fixing datetime picker

--- dashboard.php
+++ dashboard.php
@@ -12,16 +12,18 @@ if(!isset($_SESSION['id']))
         <!-- google fonts -->
         <link href='https://fonts.googleapis.com/css?family=Asap|Ubuntu:500' rel='stylesheet' type='text/css'> 
         <!-- for datepicker -->
-        <link rel="stylesheet" href="css/ui-lightness/jquery-ui-1.8.17.custom.css" type="text/css"></link>
+        <link rel="stylesheet" href="css/jquery.datetimepicker.css" type="text/css"></link>
         <!-- for colorpicker -->
         <link rel="stylesheet" href="js/color-picker/colorPicker.css" type="text/css"></link>
         
         <!-- 3rd Party -->
-        <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.22/jquery-ui.js"></script>
+        <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.1.min.js"></script>-->
+        <!--<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>-->
+        <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.22/jquery-ui.js"></script>-->
+        <!--<script type="text/javascript" src="js/jquery-ui-1.8.17.custom.min.js"></script>-->
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.3/jquery.mobile.min.js"></script>
         <script type="text/javascript" src="js/color-picker/jquery.colorPicker.js"></script>
-		<script type="text/javascript" src="js/datetimepicker.js"></script>
+		<script type="text/javascript" src="js/jquery.datetimepicker.js"></script>
                 
         <!-- Ours -->
         <script type="text/javascript" src="js/whiteboard.js" ></script>
