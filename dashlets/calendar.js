5acc0397da0c632e911f23d0c868db3e8fb6e5f3
Owen Johnson <owen.johnson@pioneer.com>
2014-08-20 20:22:06 -0500

    attempt to remember last navigated to month

--- dashlets/calendar.js
+++ dashlets/calendar.js
@@ -1,65 +1,44 @@
-$(document).ready(function ()
-{
+$(document).ready(function () {
     $("#calendar-month-view").load('/ajax/calendar-month.php?');
-    $('body').click(function (event)
-    {
-        if ($(event.target).is("#edit-appointment"))
-        {
+    $('body').click(function (event) {
+        if ($(event.target).is("#edit-appointment")) {
             var id = $("#calendar-details-div").attr("data-key");
             $("#calendar-agenda-view").slideUp();
-            $("#calendar-agenda-view").load('ajax/edit-calendar-details.php?id=' + id, function ()
-            {
+            $("#calendar-agenda-view").load('ajax/edit-calendar-details.php?id=' + id, function () {
                 $("#calendar-agenda-view").slideDown();
                 $("#calendar-month-view").slideUp();
             });
         }
 
-        if ($(event.target).is('#calendar-new'))
-        {
+        if ($(event.target).is('#calendar-new')) {
             $('#new-calendar-item').load('ajax/new-appointment-item.php');
             $('#new-calendar-item').slideDown();
             $('#color1').colorPicker();
         }
 
-        if ($(event.target).is('#delete-appointment'))
-        {
+        if ($(event.target).is('#delete-appointment')) {
             $('#calendar-month-view').slideUp();
-            $('#calendar-month-view').load('ajax/calendar-month.php', function ()
-            {
+            $('#calendar-month-view').load('ajax/calendar-month.php', function () {
                 $('#calendar-agenda-view').slideUp();
                 $('#calendar-month-view').slideDown();
             });
         }
 
-        if ($(event.target).is('#cancel-calendar-button'))
-        {
+        if ($(event.target).is('#cancel-calendar-button')) {
             $('#new-calendar-item').slideUp();
             $('#calendar-month-view').slideDown();
         }
 
-        if ($(event.target).is('#calendar-show-agenda'))
-        {
+        if ($(event.target).is('#calendar-show-agenda')) {
             $('#calendar-agenda-view').slideUp();
 
-            $('#calendar-agenda-view').load('ajax/calendar-agenda-view.php', function ()
-            {
+            $('#calendar-agenda-view').load('ajax/calendar-agenda-view.php', function () {
                 $('#calendar-agenda-view').slideDown();
                 $('#calendar-month-view').slideUp();
             });
         }
 
-        if ($(event.target).is('#calendar-show-boxes'))
-        {
-            $('#calendar-month-view').slideUp();
-            $('#calendar-month-view').load('ajax/calendar-month.php', function ()
-            {
-                $('#calendar-agenda-view').slideUp();
-                $('#calendar-month-view').slideDown();
-            });
-        }
-
-        if ($(event.target).is(".calendar-agenda-item > *") || $(event.target).is(".calendar-agenda-item") || $(event.target).is(".cal-item"))
-        {
+        if ($(event.target).is(".calendar-agenda-item > *") || $(event.target).is(".calendar-agenda-item") || $(event.target).is(".cal-item")) {
             $("#calendar-agenda-view").slideUp();
 
             var id;
@@ -70,19 +49,17 @@ $(document).ready(function ()
             else if ($(event.target).is(".cal-item"))
                 id = $(event.target).attr("data-detail-key");
 
-            $("#calendar-agenda-view").load('ajax/calendar-appointment-details.php?id=' + id, function ()
-            {
+            $("#calendar-agenda-view").load('ajax/calendar-appointment-details.php?id=' + id, function () {
                 $("#calendar-agenda-view").slideDown();
                 $("#calendar-month-view").slideUp();
             });
         }
-        if ($(event.target).is(".cal-nav-item"))
-        {
+        if ($(event.target).is(".cal-nav-item")) {
             //$('#calendar-month-view').slideUp();
             var nav;
             nav = $(event.target).attr("nav-key");
-            $("#calendar-month-view").load('ajax/calendar-month.php?'+nav, function()
-            {
+            $("#calendar-show-boxes").attr("nav-key", nav);
+            $("#calendar-month-view").load('ajax/calendar-month.php?' + nav, function () {
                 $("#calendar-agenda-view").slideUp();
                 $('#calendar-month-view').slideDown();
             });
--- dashlets/calendar.php
+++ dashlets/calendar.php
@@ -1,6 +1,6 @@
 <h3>Calendar</h3>
 <button id="calendar-show-agenda">Week</button>
-<button id="calendar-show-boxes">Month</button>
+<button id="calendar-show-boxes" class="cal-nav-item">Month</button>
 <button id="calendar-new">New</button>
 <div id="new-calendar-item"></div>
 <div id="calendar-month-view"></div>
